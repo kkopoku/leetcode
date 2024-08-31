f386a425f602353edaf33c0e030aaa4fe78eb98d
kkopoku <kk.opoku@outlook.com>
2024-08-31 19:29:11 +0000

    valid parenthesis in py

---
+++ neetcode_150/valid-parenthesis.py
@@ -0,0 +1,21 @@
+def isValid(s: str) -> bool:
+    stack = []
+    map = {
+    "}" : "{",
+    "]" : "[",
+    ")" : "(",
+    }
+
+    for i, item in enumerate(s):
+        if item not in map:
+          stack.append(s[i])
+        else:
+          if len(str) == 0:
+            return False
+          else:
+            if s[i] != map[stack[-1]]:
+              return False
+        
+        return len(stack) == 0
+
+print(isValid("{}{}{}{}[]}}"))
