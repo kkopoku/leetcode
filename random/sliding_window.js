abe6ffe18c76abfb00756e9f82a76cc781f89013
kkopoku <kk.opoku@outlook.com>
2024-09-14 23:11:08 +0000

    learning;: sliding window wip, not done

---
+++ random/sliding_window.js
@@ -0,0 +1,29 @@
+// Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+// Output: [3,3,5,5,6,7]
+// Explanation: 
+// Window position                Max
+// ---------------               -----
+// [1  3  -1] -3  5  3  6  7       3
+//  1 [3  -1  -3] 5  3  6  7       3
+//  1  3 [-1  -3  5] 3  6  7       5
+//  1  3  -1 [-3  5  3] 6  7       5
+//  1  3  -1  -3 [5  3  6] 7       6
+//  1  3  -1  -3  5 [3  6  7]      7
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+
+    let maxes = []
+    for (let i=0; i<nums.length-(k-1); i++){
+        let currentWindow = nums.slice(i,i+k)
+        maxes.push(Math.max(...currentWindow))
+    }
+    return maxes
+};
+
+nums = [1,3,-1,-3,5,3,6,7]
+console.log(maxSlidingWindow(nums, 3))
