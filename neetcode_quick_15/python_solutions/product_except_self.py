ccf127bef9394b28e287476894a497b82f873337
kkopoku <kk.opoku@outlook.com>
2025-04-23 10:57:56 +0000

    chore: product except self solved py

---
+++ neetcode_quick_15/python_solutions/product_except_self.py
@@ -0,0 +1,24 @@
+#question: https://neetcode.io/problems/products-of-array-discluding-self
+# Input: nums = [1,2,4,6]
+# Output: [48,24,12,8]
+
+from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        length = len(nums)
+        res = [1]*length
+        
+        # prefix loop ------>
+        prefix = 1 #current prod of prefix
+        for i in range(length):
+            res[i] = prefix #set the prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(length -1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+
+        return res
